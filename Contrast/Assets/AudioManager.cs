acff904bdaf406b5868ae5eb0bb65cabe0ede151
DennisBorst <dennis.borst@hotmail.nl>
2019-12-16 21:26:50 +0100

    Some Art added biatch

---
+++ Contrast/Assets/AudioManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    //Aparte audioSources
+    [SerializeField] private AudioSource footSteps;
+    [SerializeField] private AudioSource elevatorMoving;
+
+    //Samen 1 audioSource
+    [SerializeField] private AudioSource bridgeFalling;
+    [SerializeField] private AudioSource cutRope;
+    [SerializeField] private AudioSource enemyKill;
+
+    public void FootSteps()
+    {
+
+    }
+
+    public void ElevatorMoving()
+    {
+
+    }
+
+
+    #region Singleton
+    private static AudioManager instance;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    public static AudioManager Instance
+    {
+        get
+        {
+            if(instance == null)
+            {
+                instance = new AudioManager(); 
+            }
+
+            return instance;
+        }
+    }
+    #endregion
+}
