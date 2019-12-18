fd0bb3e270a5845838430955bf8773283e1ca8c3
JHunter465 <jael.vanrossum@student.hku.nl>
2019-12-18 12:15:10 +0100

    -Fixed Intro Animation and Added Ambient Sounds.

--- Contrast/Assets/Scripts/VideoTest.cs
+++ Contrast/Assets/Scripts/VideoTest.cs
@@ -7,6 +7,7 @@ public class VideoTest : MonoBehaviour
 {
     [SerializeField] private VideoClip videoClip;
     [SerializeField] private Animator anim;
+    [SerializeField] private GameObject levelAudio;
 
     // Start is called before the first frame update
     private void Start()
@@ -18,9 +19,16 @@ public class VideoTest : MonoBehaviour
     IEnumerator WaitForEndOfClip()
     {
         yield return new WaitForSeconds((float)videoClip.length);
-
+        Debug.Log("End of clip");
+        for(int i = 0; i<96; i++)
+        {
+            GetComponent<VideoPlayer>().targetCameraAlpha -= 0.01f;
+            Debug.Log("Alpha" + i);
+            yield return new WaitForSeconds(.01f);
+        }
         Debug.Log("Test");
         gameObject.SetActive(false);
+        levelAudio.SetActive(true);
         anim.enabled = true;
 
         yield return null;
