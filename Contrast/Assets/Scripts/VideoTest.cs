862e846a3a21a2b6795f6f7bb8d63a97daa4c968
DennisBorst <dennis.borst@hotmail.nl>
2019-12-18 13:04:16 +0100

    stuff

--- Contrast/Assets/Scripts/VideoTest.cs
+++ Contrast/Assets/Scripts/VideoTest.cs
@@ -2,11 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Video;
+using UnityEngine.SceneManagement;
 
 public class VideoTest : MonoBehaviour
 {
     [SerializeField] private VideoClip videoClip;
     [SerializeField] private Animator anim;
+
+    [SerializeField] private int menuScene;
 
     // Start is called before the first frame update
     private void Start()
@@ -19,10 +22,14 @@ public class VideoTest : MonoBehaviour
     {
         yield return new WaitForSeconds((float)videoClip.length);
 
-        Debug.Log("Test");
         gameObject.SetActive(false);
         anim.enabled = true;
 
         yield return null;
+    }
+
+    public void GoToMenu()
+    {
+        SceneManager.LoadScene(menuScene);
     }
 }
